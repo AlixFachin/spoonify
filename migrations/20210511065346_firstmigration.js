5016c17180ed62834e0445b5b5807afd3f2a45f1
Alix Fachin <alix.fachin@me.com>
2021-05-11 19:17:35 +0900

    Database first migration is working

---
+++ migrations/20210511065346_firstmigration.js
@@ -0,0 +1,39 @@
+
+exports.up = function(knex) {
+  return knex.raw('create extension if not exists "uuid-ossp"').then(() => 
+  Promise.all([
+    knex.schema.createTable('products', (t) => {
+      t.increments().unsigned().primary();
+      t.string('name').notNull();
+      t.text('description');
+      t.float('price').notNull();
+    } ),
+    knex.schema.createTable('users', (t) => {
+      t.uuid('id').unique().notNull().defaultTo(
+        knex.raw('uuid_generate_v4()')).primary();
+      t.string('userName');
+      t.string('fullName');
+      t.text('address');
+    }),
+    knex.schema.createTable('orders', (t) => {
+      t.increments().unsigned().primary();
+      t.uuid('userId').references('id').inTable('users');
+      t.specificType('items', 'text ARRAY');
+      t.float('deliveryFee');
+      t.float('tip');
+      t.float('totalPrice');
+      t.string('status');
+      t.datetime('timestamp');
+    })
+  ]));
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('products').dropTable('users').dropTable('orders').raw('drop extension if exists "uuid-ossp"');
+  // return Promise.all([
+  //     knex.schema.dropTable('products'),
+  //     knex.schema.dropTable('users'),
+  //     knex.schema.dropTable('orders'),
+  //     knex.raw('drop extension if exists "uuid-ossp"')
+  //   ]);
+};
