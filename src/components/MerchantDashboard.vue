f38c3db0f345efade173aeb2792f5fc55979d173
jaysutham <jaysutham@gmail.com>
2021-05-12 18:57:14 +0900

    Co-authored-by: Mark Herring <Worldonawire@users.noreply.github.com> Co-authored-by: Mattia Bonicelli <Bonicelli-Mattia@users.noreply.github.com>

--- src/components/MerchantDashboard.vue
+++ src/components/MerchantDashboard.vue
@@ -1,5 +1,29 @@
 <template >
     <div id="merchant-dashboard-container">
+        <v-card v-for="item in this.$store.state.orderList" :key="item.id">
+        <v-list-item two-line>
+            <v-list-item-avatar
+            class="fill-height grey"
+            tile
+            left
+            max-height="50"
+            size="20%"
+            color="grey">
+            <v-img color='grey' class="fill-height grey"
+                :src="require(`@/assets/${item.name}.jpg`)"
+                ></v-img>
+            </v-list-item-avatar>
+        <v-list-item-content>
+            <v-list-item-title class="mb-2"> {{item.name}} </v-list-item-title>
+            <v-list-item-subtitle>quantity:{{item.quantity}}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-spacer></v-spacer>
+        <v-list-item-content>
+            <v-list-item-title class="mb-2">¥{{item.price}}</v-list-item-title>
+            <v-list-item-subtitle></v-list-item-subtitle>
+        </v-list-item-content>
+        </v-list-item>
+        </v-card>
     </div>
 </template>
 
