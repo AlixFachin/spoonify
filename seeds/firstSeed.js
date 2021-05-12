c4c7a1ac6d5cae3048ffbf22d1fc9e1e46a86316
Michael Metcalf <michaelmetcalf52@gmail.com>
2021-05-12 13:36:06 +0900

    added three get endpoints

--- seeds/firstSeed.js
+++ seeds/firstSeed.js
@@ -5,9 +5,22 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('products').insert([
-        {name: "burger", price: 2500 , description: "tasty burger made with Kobe beef and pineapple and teriyaki sauce", image_filepath: "../src/assets"},
-        {name: "jam", price: 900 , description: "hand-made jam with local fruits produced by dedicated artisan", image_filepath: "../src/assets"},
-        {name: "pickles", price: 1200 , description: "Fermented pickles with vinegar imported from Italy", image_filepath: "../src/assets"}        
-      ]);
+        {name: "burger", price: 2500 , description: "tasty burger made with Kobe beef and pineapple and teriyaki sauce", image_filepath: "../src/assets/190524-classic-american-cheeseburger-ew-207p-2870431.jpg"},
+        {name: "chicken sandwich", price: 2200 , description: "served with lettuce, onion, pickle, and drizzled in our original sauce", image_filepath: "../src/assets/"},
+        {name: "jam", price: 900 , description: "hand-made jam with local fruits produced by dedicated artisan", image_filepath: "../src/assets/burger-sauce-fb-ig-5-scaled.jpg"},
+        {name: "pickles", price: 1200 , description: "Fermented pickles with vinegar imported from Italy", image_filepath: "../src/assets/dill-pickles-14132-1.jpg"},
+        {name: "french fries", price: 500 , description: "made fresh daily, salted with sea salt", image_filepath: "../src/assets/"},
+        {name: "curly fires", price: 600 , description: "made fresh daily, seasoned with our original seasoning", image_filepath: "../src/assets/"},
+        {name: "brownie", price: 500 , description: "delicious homemade fudge brownies", image_filepath: "../src/assets/"},   
+      ]).then(() => {
+        return knex('users').del()
+        .then(function () {
+          return knex('users').insert([
+          {userName: "bigrigs", fullName: "Jim Riggs", address: "512 Honk Honk Ln."},
+          {userName: "catburger", fullName: "Tiger Beef", address: "The alley behind your house"},
+          {userName: "frillylily123", fullName: "Janet Planet", address: "645 Main St, Apt D"}
+        ]);
+      });
     });
+  });
 };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const knexLib = require('knex');
 const knexConfig = require('../knexfile.js');
 const path = require('path');
+const e = require('express');
+const { response } = require('express');
 
 const db = knexLib( (process.env.NODE_ENV && process.env.NODE_ENV === 'production' ) ?  
   knexConfig.production : 
@@ -9,13 +11,16 @@ const db = knexLib( (process.env.NODE_ENV && process.env.NODE_ENV === 'productio
 
 const app = express();
 
+app.use(express.json());
+
 // Static files for front-end
 app.use('/',express.static(path.join(__dirname,'../dist')));
 console.log(path.join(__dirname,'../dist'));
-// Endpoints
 
+// Endpoints
 app.get('/api/products', (request, responseHandler) => {
   db('products').select('*').then((dbData) => {
+    console.log(dbData);
     responseHandler.status(200).send(dbData);
   }).catch((error) => {
     console.error(error);
@@ -37,13 +42,58 @@ app.get('/api/product/:id', (request, responseHandler) => {
   })
 });
 
-app.get('api/user/:id', (request, respsonseHandler) => {
+app.get('/api/user/:id', (request, responseHandler) => {
   db('users').select('*').where('id', request.params.id).then((dbData) => {
     if (dbData.length === 0) {
       responseHandler.status(404).send(`Cannot find user with id ${request.params.user} in the DB`);
     } else {
       responseHandler.status(200).send(dbData[0]);
     }
+  }).catch((error) => {
+    responseHandler.status(500).send(`Server error ${error}`);
+  });
+});
+
+app.get('/api/order/:id', (request, responseHandler) => {
+  db('orders').select('*').where('id', request.params.id).then((dbData) => {
+    if (dbData.length === 0) {
+      responseHandler.status(404).send(`Cannot find order with id ${request.params.id} in the DB`);
+    } else {
+      responseHandler.status(200).send(dbData[0]);
+    }
+  }).catch((error) => {
+    responseHandler.status(500).send(`Server error ${error}`);
+  });
+});
+
+app.get('/api/user/:id/orders', (request, responseHandler) => {
+  db('users').select('*').where('id', request.params.id).then((dbData) => {
+    if (dbData.length === 0) {
+      responseHandler.status(404).send(`Cannot find user with id ${request.params.id} in the DB`);
+    } else {
+      responseHandler.status(200).send(dbData[0]);
+    }
+  }).catch((error) => {
+    responseHandler.status(500).send(`Server error ${error}`);
+  });
+});
+
+app.post('/api/user', (request, responseHandler) => {
+  console.log(request.body);
+  db('users').insert(
+    { userName: request.body.userName, fullName: request.body.fullName, address: request.body.address },
+  ).then((dbData) => {
+    if (request.body.userName === "") {
+      responseHandler.status(400).send("Please enter a username");
+    } else if (request.body.fullName ==="") {
+      responseHandler.status(400).send("Please enter a name");
+    } else if (request.body.address === "") {
+      responseHandler.status(400).send("Please enter an address");
+    } else {
+      responseHandler.status(200).send(response.body);
+    }
+  }).catch((error) => {
+    responseHandler.status(500).send(`Server error ${error}`);
   });
 });
 
