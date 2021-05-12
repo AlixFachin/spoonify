ecf5164b08372f60621a0d605d28b0f7409f2351
Jay <jaysutham@gmail.com>
2021-05-12 09:48:44 +0900

    added ui

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,92 @@
+ 
 <template>
-  <v-card  class="xl">
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant.sync="mini"
-      permanent
-      class="xl"
-    >
-      <v-list-item class="px-5">
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-        </v-list-item-avatar>
+  <v-app id="spoonify">
+     <v-navigation-drawer
+        permanent
+        expand-on-hover
+        v-model='drawer'
+        app
+      >
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/25.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
 
-        <v-list-item-title>John Leider</v-list-item-title>
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
 
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
+        <v-divider></v-divider>
+
+        <v-list
+          nav
+          dense
         >
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon
+              color="teal darken-2">mdi-account-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account Info</v-list-item-title>
+          </v-list-item>
+          <v-list-item link @click="testfunction">
+            <v-list-item-icon>
+              <v-icon
+              color="red darken-2">mdi-credit-card</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Purchase History</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon
+              color="purple darken-2">mdi-cart</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shopping Cart</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon
+              color="yellow darken-2">mdi-basket</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Main Menu</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-      <v-divider></v-divider>
+    <v-app-bar class="lightgrey" app>
+      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+    
+      <v-toolbar-title class="font-weight-black" center>Spoonify</v-toolbar-title>
+    </v-app-bar>
 
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+    <v-main>
+      <PurchaseHistory />
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import PurchaseHistory from './components/PurchaseHistory'
+
   export default {
-    data () {
-      return {
-        drawer: true,
-        items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
-        ],
-        mini: true,
+    components:{
+      PurchaseHistory
+    },
+    data: () => ({ 
+      drawer: null,
+     }),
+    methods: {
+      testfunction(){
+        console.log('test')
       }
     },
   }
---
+++ src/components/PurchaseHistory.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card class="mx-auto mt-1" max-width="98%" elevation="4" rounded>
+    <v-list-item>
+    <v-list-item-title class="text-truncate"> Royale with Cheese </v-list-item-title>
+    <v-list-item-avatar
+    tile
+    size="20%"
+    color="grey">
+      <v-img class="fill-height"
+      
+  lazy-src="https://picsum.photos/id/11/10/6"
+  src="https://picsum.photos/id/11/500/300"
+></v-img>
+    </v-list-item-avatar>
+    </v-list-item>
+    
+    <v-card-actions>
+      <v-btn color="blue white--text" small elevation="2" @click="addToCart"> Add to Cart </v-btn>
+      <v-spacer></v-spacer>
+       <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+         [JULES]
+
+They don't call it a Quarter Pounder with Cheese?
+No, they got the metric system there, they wouldn't know what the fuck a Quarter Pounder is.
+What'd they call it?
+They call it Royale with Cheese.
+Royale with Cheese. What'd they call a Big Mac?
+Big Mac's a Big Mac, but they call it Le Big Mac.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+<script>
+  export default {
+    methods:{
+      addToCart(){
+        console.log('item added to cart!')
+      }
+    },
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
