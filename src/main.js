58b318fb80f0ac7d4de2a8c54bf301e63f90c11d
Mark <markc.herring@gmail.com>
2021-05-13 11:48:03 +0900

    Mark temp

--- src/main.js
+++ src/main.js
@@ -2,11 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from "./store"
 import vuetify from './plugins/vuetify'
+import router from 'vue-router'
+
+import { domain, clientId, audience } from "../auth_config.json";
+import { Auth0Plugin } from "@/auth/auth0-plugin";
+
+
+
+// Install the authentication plugin
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
+  },
+});
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
