e338678ba783182bb808c513dee1b49bec6a47b6
Alix Fachin <alix.fachin@me.com>
2021-05-14 10:44:13 +0900

    Color background on the app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
  
 <template>
-<v-app id="spoonify">
+<v-app id="spoonify" style="background: linear-gradient(to right, #FFD194, #D1913C)">
      <v-navigation-drawer
         permanent
         expand-on-hover
@@ -177,11 +177,6 @@ export default {
   margin-top: 60px;
 }
 
-html {
-  background: #D1913C;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #FFD194, #D1913C);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #FFD194, #D1913C); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-}
 
 </style>
 
